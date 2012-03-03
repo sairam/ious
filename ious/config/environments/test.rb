fa5eee12f7acadddbf67cc2e2cf58648d8a8828a
Sairam Kunala <sairam.kunala@gmail.com>
2012-03-03 15:07:30 +0530

    Configuring spork,autotest,cucumber,rspec

--- ious/config/environments/test.rb
+++ ious/config/environments/test.rb
@@ -1,11 +1,14 @@
 Ious::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  ENV['AUTOFEATURE'] = "true"
+  ENV['RSPEC'] = "true"
+
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
